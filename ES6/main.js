49e32270fb3281b1dfa6f155b739e07dbad00d97
oakley <oakleypeavler@gmail.com>
2018-09-06 00:47:17 -0400

    collisions kinda getting there

--- ES6/main.js
+++ ES6/main.js
@@ -29,6 +29,7 @@
   function clickEvent(e) {
     e = e || window.event;
     worldEntities.push(new SquareEntity({
+      name: Math.random()*9123482,
       x: Math.floor(Math.random()*canvas.width), 
       y: Math.floor(Math.random()*canvas.height), 
       xvector: Math.floor(Math.random()*8) - Math.floor(Math.random()*32),
@@ -66,7 +67,9 @@
   
   class SquareEntity {
     constructor({x = 10, y = 10, width = 30, height = 30, fillStyle = '#000000', speedx = 80, speedy = 90, 
-      xvector = 0, yvector = 0, friction = 0.8, gravity = 98, bounciness = 0.2}) {
+      xvector = 0, yvector = 0, friction = 0.8, gravity = 98, bounciness = 0.2, name = 'entity'}) {
+
+      // yes there should be accessors, but this is javascript, and neither me nor the browser cares
       this.x = x;
       this.y = y;
       this.width = width;
@@ -79,6 +82,7 @@
       this.gravity = gravity;
       this.friction = friction;
       this.bounciness = bounciness;
+      this.name = name;
     }
   
     draw(ctx) {
@@ -122,15 +126,70 @@
     }
 
     doesIntersect(entity) {
+      if(entity instanceof SquareEntity) {
+        const sides = {
+          top: (x, y, w, h) => [[x, y], [x+w, y]],
+          left: (x, y, w, h) => [[x, y], [x, y+h]],
+          right: (x, y, w, h) => [[x+w, y], [x+w, y+h]],
+          bottom: (x, y, w, h) => [[x, y+h], [x+w, y+h]]
+        }
 
+        // (a, b) -> (c, d) intersection with line (p, q) -> (r, s)
+        for (const [side, [ [[a, b], [c, d]], [[p, q], [r, s]] ] ] of 
+          Object.keys(sides).map(key => 
+            [ key, 
+              [ sides[key](entity.x, entity.y, entity.width, entity.height), 
+                sides[key](this.x, this.y, this.width, this.height) ] ]
+          )) {
+
+          // console.log(`checking ${entity.name} against ${this.name}`);
+
+          // console.log({
+          //   side, a, b, c, d, p, q, r, s
+          // });
+
+          const det = (c-a) * (s-q) - (r-p) * (d-b);
+
+          if(det === 0) { // parallel
+            // console.log({s, d, r, c})
+            // if (Math.abs(s - d) < 0.9 && (Math.abs(r-c) <= this.width || Math.abs(p-a) <= this.width)) {
+            //   return side;
+            // } else if (Math.abs(r - c) < 0.01 && (Math.abs(s-d) <= this.height || Math.abs(q-b) <= this.height)) {
+            //   return side;
+            // }
+            if (c >= p && c <= r && b >= q) { // right side touches left
+              return 'right';
+            } 
+            else if (a <= r && p <= c && b >= q) {
+              return 'left';
+            }
+            else if (d + entity.height >= q + 5 && ((c >= p && c <= r) || (a <= r && p <= c))) {
+              return 'bottom';
+            }
+            else if (b + this.height >= q + 5 && ((c >= p && c <= r) || (a <= r && p <= c))) {
+              return 'top';
+            }
+
+          }
+
+          const lambda = ((s-q) * (r-a) + (p - r) * (s-b)) / det;
+          const gamma = ((b-d) * (r-a) + (c-a) * (s-b)) / det;
+
+          if ((-0.01 < lambda && lambda < 1.01) && (-0.01 < gamma && gamma < 1.01)) {
+            return side;
+          }
+        }
+      }
     }
+
   }
 
   let worldEntities = [ ];
   let defaultTarget = { };
 
   function initWorld() {
-    worldEntities.push(new SquareEntity({fillStyle: '#FF0000', width: 30, height: 30}));
+    worldEntities.push(new SquareEntity({fillStyle: '#FF0000', name: 'ent1', width: 30, height: 30}));
+    worldEntities.push(new SquareEntity({fillStyle: '#777', name: 'ent2', width: 50, height: 50, x: 500, y: 300}));
     defaultTarget = worldEntities[0];
   }
 
@@ -158,11 +217,36 @@
   
   function applyEntityCollisions(entities) {
     entities.forEach(entity => {
-      const collidingEntities = entities.filter(target => entity.doesIntersect(target));
+      const collidingEntities = entities.filter(t => entity.name !== t.name)
+        .map(target => [entity.doesIntersect(target), target]).filter(e => !!e[0]);
 
-      collidingEntities.forEach(target => {
-        entity.xvector -= (target.xvector*target.bounciness*entity.bounciness);
-        entity.yvector -= (target.yvector*target.bounciness*entity.bounciness);
+        if (collidingEntities.length) console.log(collidingEntities);
+      // console.log({collidingEntities});
+
+      // let a = collidingEntities.find(e => !!e);
+      collidingEntities.forEach(([side, target]) => {
+        const side_axis_dir = {
+          top: ['y', -1],
+          bottom: ['y', +1],
+          left: ['x', -1],
+          right: ['x', +1]
+        }
+
+        if(side === 'right' || side === 'left') {
+          // entity.x = 'right' ? entity.x-target.width + 0.01 : target.x - 0.01;
+          entity[`${side_axis_dir[side][0]}vector`] += 
+            (side_axis_dir[side][1])*(target[`${side_axis_dir[side][0]}vector`]);
+        }
+      
+        if(side === 'top') {
+          // target[`${side_axis_dir[side][0]}vector`] += 
+          // (side_axis_dir[side][1])*(entity[`${side_axis_dir[side][0]}vector`]);
+          
+          target[`${side_axis_dir[side][0]}vector`] = -(target.yvector*target.bounciness);
+          target[side_axis_dir[side][0]] = entity.y-target.height-0.2; //put the entity right next to 0, otherwise it's colliding
+        } 
+        // entity.xvector -= (target.xvector*target.bounciness*entity.bounciness);
+        // entity.yvector -= (target.yvector*target.bounciness*entity.bounciness);
       });
     });
   }
